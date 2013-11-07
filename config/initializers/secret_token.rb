985cc9db6ddc1944078fcc2ccc7746f743ae7886
Heidi Evans <heidi.evans@ryerson.ca>
2013-11-06 19:21:28 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeForward::Application.config.secret_token = 'a378c0b12d4710936545fe8be1a4f869018924ffb150c462fdb41586c36cc3218dd7ac9eabec6279089d73d65b506ab503022881b35d1b714bec03d044bce9d2'
